a694b289020f1b8b0ea023aea163a555b3ed5011
moonilson <underground@openobject.net>
2020-11-19 16:49:59 +0900

    [moonilson] props 정리

--- src/views/Statement.vue
+++ src/views/Statement.vue
@@ -1,18 +1,21 @@
 <template>
   <v-container>
-    <v-row
-      class="px-2 py-3"
-    >
-      <v-col
-        cols=7
-      >
-        <!-- 사용내역 타이틀 -->
-        <v-card-title class="justify-center">
-          법인카드 사용내역서
-        </v-card-title>
+    <v-row>
+      <v-col cols=7>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-card-title class="justify-center" v-bind="attrs" v-on="on">
+              법인카드 사용내역서
+            </v-card-title>
+            <v-alert dense outlined dismissible type="error" v-show="isLoadingMaxWait">
+              님아 데이터를 가져오는데 <strong>최대대기 시간 2초</strong>가 초과됐습니다. <strong>새로고침</strong> 해주셈 ㄱㄱㄱㄱ
+            </v-alert>
+          </template>
+          <span>사용내역 항목을 클릭하면 상세내역 확인 가능합니다.</span>
+        </v-tooltip>
 
         <!-- 구분선 -->
-        <v-divider class="mx-4"></v-divider>
+        <v-divider></v-divider>
 
         <!-- 사용내역 테이블 -->
         <v-data-table
@@ -41,133 +44,103 @@
       </v-col>
 
       <!-- 구분선 -->
-      <v-col
-        cols=1>
-        <v-divider vertical class="pl-10"></v-divider>
+      <v-col cols=1>
+        <v-divider vertical class="pl-14"></v-divider>
       </v-col>
 
       <!-- 사용내역 입력란 -->
-      <v-col
-        cols=4
-      >
-        <v-card>
-          <v-card-title>Summary</v-card-title>
-          <v-card-text>사용 총합계 : {{ getTotalUsedAmount }}원</v-card-text>
-          <v-card-text>잔액: {{ getTotalBalanceAmount }}원</v-card-text>
-        </v-card>
-        <v-card>
-          <v-card-title>
-            <h5 class="display-5">사용내역 입력</h5>
-          </v-card-title>
-          <v-card-text>
-            <v-form>
-              <!-- 사용일시 -->
-              <v-menu
-                ref="menu"
-                v-model="menu"
-                :close-on-content-click="false"
-                :return-value.sync="usedDate"
-                transition="scale-transition"
-                offset-y
-                min-width="290px"
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
-                    v-model="usedDate"
-                    label="사용일시"
-                    prepend-icon="mdi-calendar"
-                    readonly
-                    v-bind="attrs"
-                    v-on="on"
-                  ></v-text-field>
-                </template>
-                <v-date-picker
-                  v-model="usedDate"
-                  no-title
-                  scrollable
-                  :max="todayDate"
+      <v-row flex-direction="column">
+
+        <v-col cols=12>
+          <v-card>
+            <v-card-title>Summary</v-card-title>
+            <v-divider></v-divider>
+            <v-card-text>사용 총합계 : {{ getTotalUsedAmount }}원</v-card-text>
+            <v-card-text>잔액 : {{ getTotalBalanceAmount }}원</v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col cols=12>
+          <v-card>
+            <v-card-title>
+              <h5 class="display-5">사용내역 입력</h5>
+            </v-card-title>
+            <v-divider></v-divider>
+            <v-card-text>
+              <v-form>
+                <!-- 사용일시 -->
+                <v-menu
+                  ref="menu"
+                  v-model="menu"
+                  :close-on-content-click="false"
+                  :return-value.sync="usedDate"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="290px"
                 >
-                  <v-spacer></v-spacer>
-                  <v-btn
-                    text
-                    color="primary"
-                    @click="menu = false"
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-model="usedDate"
+                      label="사용일시"
+                      prepend-icon="mdi-calendar"
+                      readonly
+                      v-bind="attrs"
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker
+                    v-model="usedDate"
+                    no-title
+                    scrollable
+                    :max="todayDate"
                   >
-                    Cancel
-                  </v-btn>
-                  <v-btn
-                    text
-                    color="primary"
-                    @click="$refs.menu.save(usedDate)"
-                  >
-                    OK
-                  </v-btn>
-                </v-date-picker>
-              </v-menu>
-              <!-- 사용일시 -->
+                    <v-spacer></v-spacer>
+                    <v-btn text color="primary" @click="menu = false">
+                      Cancel
+                    </v-btn>
+                    <v-btn text color="primary" @click="$refs.menu.save(usedDate)">
+                      OK
+                    </v-btn>
+                  </v-date-picker>
+                </v-menu>
+                <!-- 사용일시 -->
 
-              <!-- 분류 -->
-              <v-autocomplete
-                v-model="category"
-                :items="categoryList"
-                dense
-                filled
-                label="분류"
-                prepend-icon="mdi-sort-reverse-variant"
-              ></v-autocomplete>
+                <!-- 분류 -->
+                <v-autocomplete
+                  v-model="category"
+                  :items="categoryList"
+                  dense
+                  filled
+                  label="분류"
+                  prepend-icon="mdi-sort-reverse-variant"
+                ></v-autocomplete>
 
-              <v-text-field
-                label="고객사"
-                prepend-icon="mdi-target"
-                v-model="customer"
-              />
-              <v-text-field
-                label="사용목적 또는 사유(상세히)"
-                prepend-icon="mdi-note-text-outline"
-                v-model="purpose"
-              />
-              <v-text-field
-                label="사용금액"
-                prepend-icon="mdi-currency-krw"
-                v-model="amount"
-              />
-              <v-text-field
-                label="기타(메모)"
-                prepend-icon="mdi-note-plus-outline"
-                v-model="memo"
-              />
-            </v-form>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn
+                <v-text-field label="고객사" prepend-icon="mdi-target" v-model="customer"/>
+                <v-text-field label="사용목적 또는 사유(상세히)" prepend-icon="mdi-note-text-outline"
+                  v-model="purpose"/>
+                <v-text-field label="사용금액" prepend-icon="mdi-currency-krw" v-model="amount"/>
+                <v-text-field label="기타(메모)" prepend-icon="mdi-note-plus-outline" v-model="memo"/>
+              </v-form>
+            </v-card-text>
 
-              @click="register"
-            >Register
-            </v-btn>
-
-            <v-spacer></v-spacer>
-
-            <v-btn
-
-              @click="initInputFields"
-            >Reset
-            </v-btn>
-
-          </v-card-actions>
-        </v-card>
-      </v-col>
+            <v-card-actions>
+              <v-btn @click="register">Register</v-btn>
+              <v-spacer></v-spacer>
+              <v-btn @click="initInputFields">Reset</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-row>
 
     <!-- 분류별 합계 테이블 -->
-    <v-row
-      class="px-2 py-3"
-    >
-      <v-col
-        cols="7">
+    <v-row class="px-2 py-3">
+      <v-col cols="7">
         <v-card>
           <v-card-title>
             <h5 class="display-5">분류별 합계</h5>
           </v-card-title>
+          <v-divider></v-divider>
           <v-data-table
             :headers="headers_categorySummary"
             :items="getCategorySumList"
@@ -183,7 +156,6 @@
         </v-card>
       </v-col>
     </v-row>
-
   </v-container>
 </template>
 
@@ -208,6 +180,7 @@ export default {
     memo: '',
     menu: false,
     categoryList: ['식사', '간식', '접대', '비품', '회식', '기타'],
+    isLoadingMaxWait: false,
     headers: [
       {
         text: '사용일시',
@@ -260,10 +233,13 @@ export default {
   },
   mounted() {
     this.getHistory();
+    setTimeout(() => {
+      this.isLoadingMaxWait = true;
+    }, 2000);
   },
   computed: {
     isLoading() {
-      return this.historyList.length <= 0;
+      return (this.historyList.length <= 0 && !this.isLoadingMaxWait);
     },
     getTotalBalanceAmount() {
       return utils.numberWithCommas(this.totalBalanceAmount);
