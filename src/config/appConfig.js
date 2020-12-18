1214360402bd02ecea7d38f8f9b60324d152fc3b
moonilson <underground@openobject.net>
2020-12-18 17:36:23 +0900

    [moonilson] 웹서버 설정 주석 내용 변경

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -4,7 +4,7 @@ const appConfig = {
   /**
    * Local 환경에서 개발 시에는 server 환경을 localhost:3001을 바라본다.
    * corp_server를 Local 에서 실행 시 port 3001로 실행이 됨
-   * 배포 버전은 IPTV DEV FTP 서버인 http://114.202.216.45 환경으로 설정
+   * 배포 버전은 IPTV FTP 서버인 http://221.139.81.196  내 virtualBox(ubuntu)환경에서 동작한다.
    */
   serverIP: LOCAL_DEV ? 'http://localhost:3001' : 'http://221.139.81.196:3001',
   isLogin: false, // 개발용 인증 여부
