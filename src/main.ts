973fb5d899ddc629a64244d606a5c5a3d12b3ee9
lee-mh <lee-mh@openobject.net>
2021-02-16 16:52:08 +0900

    [lee-mh] typescript 적용 중

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+// import Vue from 'vue';
+// import Fragment from 'vue-fragment';
+// import authService from '@/plugins/auth';
+// import App from './App.vue';
+// import router from './router';
+// import store from './store';
+// import 'nprogress/nprogress.css';
+// import vuetify from './plugins/vuetify';
+
+// Vue.config.productionTip = false;
+// Vue.use(Fragment.Plugin);
+
+// let mount = null;
+
+// authService.setStateCallback((user) => {
+//   console.log('loginUser:', user ? user.email : 'null');
+//   authService.setUser(user);
+
+//   if (!mount) {
+//     mount = new Vue({
+//       router,
+//       store,
+//       vuetify,
+//       render: (h) => h(App),
+//     }).$mount('#app');
+//   }
+// });
